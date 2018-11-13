288f2c5db66355c335dc09c148c2af836a8812e6
Isaac Kinley <isaac.kinley@gmail.com>
2018-11-13 00:44:47 -0500

    More tidying

--- UI_getfilterinfo.m
+++ UI_getfilterinfo.m
@@ -1,50 +1,64 @@
 function [filterType, smoothN] = UI_getfilterinfo(EYE)
 
-%  Inputs
+%   Inputs
 % EYE--struct array
-%  Outputs
+%   Outputs
 
-FilterTypes = {'Median' 'Mean' 'Gaussian kernel'};
+[filterType, smoothN] = filterquery;
 
-Question = 'Which type of moving average?';
-filterType = questdlg(Question,Question,FilterTypes{1},FilterTypes{2},FilterTypes{3},FilterTypes{1});
-Question = 'Average of how many points on either side?';
-Answer = inputdlg(Question,Question,1,{'8'});
-smoothN = str2double(Answer);
-
-figure('units','normalized','outerposition',[0 0 1 1]);
+figure('units', 'normalized', 'outerposition', [0 0 1 1])
+hold on
 
 while true
-    % Show a different 3 seconds of data each time
     dataIdx = randi(numel(EYE));
     permData = deal(EYE(dataIdx).data);
     tempData = applyeyefilter(EYE(dataIdx), filterType, smoothN);
     
+    nSecondsToPlot = 5; % Number of seconds of data to plot
+    start = randi(length(tempData.left) - nSecondsToPlot*EYE(1).srate);
+    latencies = start:(start + nSecondsToPlot*EYE.srate);
     
-    nSeconds = 5; % Number of seconds of data to plot
-    start = randi(length(tempData.left)-nSeconds*EYE(1).srate);
-    latencies = start:(start+nSeconds*EYE.srate); % Plot 5 random seconds
-    clf; hold on
-    plot(tempData.right(latencies),'r'); plot(permData.right(latencies),'r:')
-    plot(tempData.left(latencies),'b'); plot(permData.left(latencies),'b:')
+    clf
+    plot(tempData.right(latencies), 'r')
+    plot(permData.right(latencies), 'r:')
+    plot(tempData.left(latencies), 'b')
+    plot(permData.left(latencies), 'b:')
     legend({'Right eye smoothed' 'Right eye original' 'Left eye smoothed' 'Left eye original'})
-    title(sprintf('Moving %s of %d points on either side (%d seconds of data)',filterType,smoothN,nSeconds));
+    title(sprintf('Moving %s of %d points on either side (%d seconds of data)', filterType, smoothN, nSecondsToPlot));
     xlim([1 numel(latencies)]);
     
-    Accepted = questdlg('Is this smoothing acceptable?', ...
-        'Accept this smoothing?', ...
-        'Yes','See a different epoch','Try different smoothing','See a different epoch');
+    Accepted = questdlg('Is this smoothing acceptable?',...
+        'Accept this smoothing?',...
+        'Yes', 'See a different epoch', 'Try different smoothing', 'See a different epoch');
     if strcmp(Accepted,'Yes')
-        fprintf('User accepted smoothing parameter %d\n',smoothN);
-       close; break
+        close
+        return
     elseif strcmp(Accepted,'Try different smoothing')
-        Question = 'Which type of moving average?';
-        filterType = questdlg(Question,Question,...
-            FilterTypes{1},FilterTypes{2},FilterTypes{3},filterType);
-        Question = 'Average of how many points on either side?';
-        Answer = inputdlg(Question,Question,1,{num2str(smoothN)});
-        smoothN = str2double(Answer);
-        fprintf('User switched smoothing parameter to %d\n',smoothN);
+        [filterType, smoothN] = filterquery(filterType, smoothN);
     end
 end
+
+end
+
+function [filterType, smoothN] = filterquery(varargin)
+
+filterOptions = {'Median' 'Mean' 'Gaussian kernel'};
+
+if nargin < 1
+    defaultFilterType = 'Median';
+else
+    defaultFilterType = varargin{1};
+end
+
+if nargin < 2
+    defaultSmoothN = {'8'};
+else
+    defaultSmoothN = {num2str(varargin{2})};
+end
+
+q = 'Which type of moving average?';
+filterType = questdlg(q, q, filterOptions{:}, defaultFilterType);
+q = 'Average of how many points on either side?';
+smoothN = str2double(inputdlg(q, q, 1, defaultSmoothN));
+
 end
