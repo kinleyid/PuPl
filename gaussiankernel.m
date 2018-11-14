a2c4f992c64efee4d621f95f6c571f68cfee822c
Isaac Kinley <isaac.kinley@gmail.com>
2018-11-14 11:06:27 -0500

    Bug fixes

--- gaussiankernel.m
+++ gaussiankernel.m
@@ -5,8 +5,8 @@ if numel(v) > 1
     x = (1:numel(v)) - (numel(v) + 1)/2;
     s = floor(numel(v)/2);
     Gau = exp(-((((x)/(s/3)).^2)));
-    y = sum(Gau(:).*v(:),'omitnan');
-    y = y/sum(Gau(~isnan(Gau)));
+    y = sum(Gau(:).*v(:), 'omitnan');
+    y = y / sum(Gau(~isnan(v)));
 end
 
 end
