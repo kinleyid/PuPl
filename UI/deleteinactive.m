418b4a0a1079368c884743b18fb76d1974609df4
Isaac Kinley <isaac.kinley@gmail.com>
2018-12-04 10:31:37 -0500

    Delete inactive rather than activce

---
+++ UI/deleteinactive.m
@@ -0,0 +1,24 @@
+function deletineactive(dataType)
+
+global userInterface
+
+switch dataType
+    case 'eye data'
+        global eyeData
+        currIdx = ~userInterface.UserData.activeEyeDataIdx;
+        for currData = reshape(eyeData(currIdx), 1, [])
+            fprintf('Removing %s\n', currData.name);
+        end
+        eyeData(currIdx) = [];
+        userInterface.UserData.activeEyeDataIdx(currIdx) = [];
+    case 'event logs'
+        global eventLogs
+        currIdx = ~userInterface.UserData.activeEventLogsIdx
+        for currData = reshape(eventLogs(currIdx), 1, [])
+            fprintf('Removing %s\n', currData.name);
+        end
+        eventLogs(currIdx) = [];
+        userInterface.UserData.activeEventLogsIdx(currIdx) = [];
+end
+
+end
